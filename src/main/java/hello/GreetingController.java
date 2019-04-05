1eb6b97e53f3bef4e98eff0b27d929005eed16bc
Sundaramurthy <ssundaramurthy@deloitte.com>
2019-04-05 09:53:59 -0400

    TestCommit

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -13,7 +13,8 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
+       System.out.println("INSIDE");
+    	return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
 }
